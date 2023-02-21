b93f65d210efebcbd57be509a88619c2a6b2a5e5
KenEkm <kenankeni@web.de>
2023-02-22 00:31:19 +0100

    implementing form to add and display new authors

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,39 @@
 const express = require("express")
 const router = express.Router()
+const Author = require("../models/author")
 
 //all authors
-router.get("/", (req, res) => {
-    res.render("authors/index")
+router.get("/", async (req, res) => {
+    try {
+        const authors = await Author.find({})
+        res.render("authors/index", { authors: authors })
+    } catch {
+        res.redirect("/")
+    }
 })
 
 //new author route
 router.get("/new", (req, res) => {
-    res.render("authors/new")
+    res.render("authors/new", { author: new Author() })
 })
 
 //create author route
-router.post("/", (req, res) => {
-    res.send("create")
+router.post("/", async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`author/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch (err) {
+        console.log(err)
+        res.render("authors/new", {
+            author: author,
+            errorMessage: "Error creating Author"
+        })
+    }
 })
 
 module.exports = router
